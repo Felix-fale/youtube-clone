2903004a47cb06510f4ffe22bf85a205e3d1d8d3
Felix-fale <falefelix23@gmail.com>
2022-12-30 22:46:41 +0000

    feat: add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Search } from "@mui/icons-material";
+import { Stack } from "@mui/system";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import SearchBar from "./SearchBar";
+
+
+function Navbar(props) {
+    return (
+        <Stack
+            direction='row'
+            alignItems="center"
+            p={2}
+            sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' }}
+        >
+            <Link to="/" styles= {{display: 'flex', alignItems: "center"}}>
+                <img src={logo} alt="logo" height={45} />
+            </Link>
+            <SearchBar/>
+        </Stack>
+    );
+}
+
+export default Navbar;
