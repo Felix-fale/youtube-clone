3a5d90f8a3a6ac47e056278616e7e277aa73cde7
Felix-fale <falefelix23@gmail.com>
2022-12-31 23:38:21 +0000

    feat: add VideoCard content

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChannelCard(props) {
+    return (
+        <div>
+            ChannelCard
+        </div>
+    );
+}
+
+export default ChannelCard;
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import { Box, Stack } from "@mui/system";
+import {VideoCard, ChannelCard} from "./";
 
-function Videos({videos}) {
-    
-    console.log(videos);
+function Videos({ videos }) {
+
 
     return (
-        <div>
-            Videos
-        </div>
+        <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
+            {videos.map((item, idx) => (
+                <Box key={idx}>
+                    {item.id.videoId && <VideoCard video={item} />}
+                    {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
+                </Box>
+            ))}
+        </Stack>
     );
 }
 
