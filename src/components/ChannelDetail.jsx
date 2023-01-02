5fc3258919afeb75241dd0bbe161575253e1de2b
Felix-fale <falefelix23@gmail.com>
2023-01-02 01:23:38 +0000

    feat: add favicon

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import { Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos, ChannelCard } from './'
+
 
 function ChannelDetail() {
+    const [channelDetail, setChannelDetail] = useState(null)
+
+    const { id } = useParams();
+
+    console.log(channelDetail);
+
+    useEffect(() => {
+        fetchFromAPI(`channels?part="snippet&id=${id}`)
+            .then((data) => setChannelDetail(data?.items[0]))
+    }, [id])
+
     return (
         <div>
-            ChannelDetail
+            {id}
         </div>
     );
 }
